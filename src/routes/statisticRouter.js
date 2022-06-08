b58589e2854ba617fcfad7fefd0159ad77879110
ThienHN <thienviva7761311@gmail.com>
2022-06-09 01:37:41 +0700

    [ThienHN] Fix Bug Firebase

--- src/routes/statisticRouter.js
+++ src/routes/statisticRouter.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const Controller = require('../controllers/statistic.controller')
 const router = express.Router()
-const Validate = require("../validators")
 const { checkRole } = require('../middleware/checkRole.middleware')
 const { defaultRoles } = require('../config/defineModel')
 const jwtServices = require("../services/jwt.services")
